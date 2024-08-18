f1915f93f115b8aac0f8d7c6de857522fe22b101
DaviLucianoSilva <lucianodavi160@gmail.com>
2024-08-17 23:32:38 -0300

    Segundo Comentario

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
+
+
 class heroi{
-    constructor (nome, idade, tipo, ataque){
+    constructor (nome, idade, tipo){
         this.nome = nome
         this.idade = idade 
         this.tipo = tipo 
-        this.ataque = ataque
         
     }
     
 
-    atacar(){
-console.log("O " + this.tipo + " Atacou usando " + this.ataque)
+    atacar(){ 
+        let exibirAtaque = ""
+        if (this.tipo === "mago" || this.tipo === "Mago" ){
+            exibirAtaque = "Magia"
+        }else if (this.tipo === "guerreiro" || this.tipo === "Guerreiro"){
+            exibirAtaque = "Espada"
+        }else if (this.tipo === "monge" || this.tipo === "Monge"){
+            exibirAtaque = "Artes Marciais"
+        }else if(this.tipo === "ninja" || this.tipo === "Ninja"){
+            exibirAtaque = "shuriken"
+        }else{
+            console.error ("Erro")
+            return 
+        }
+
+        console.log("O " + this.tipo + " Atacou usando " + exibirAtaque )
     }
     
 }
 
-let heroiDoCastelo = new heroi("Davi", 20, "Ninja", "Shuriken")
+let heroiDoCastelo = new heroi("Davi", 20, "Ninja")
 
 heroiDoCastelo.atacar()
